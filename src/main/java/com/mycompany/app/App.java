db8ef5e328c77ba62ac383ff42a571690cd200b1
CalvinC99 <calvin.cheam@keysight.com>
2025-04-09 16:48:48 +0800

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is so exciting.");
+        System.out.println(""Make a change to demonstrate Poll SCM.");
     }
 
     public String getMessage() {
