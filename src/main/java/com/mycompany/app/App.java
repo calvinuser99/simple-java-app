16f22c3a0b95138569d0692241199a4b0b363309
CalvinC99 <calvin.cheam@keysight.com>
2025-04-09 16:07:22 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello there ! I am simple Java app. Jenkins is awesome. I am using Jenkins !!!");
+        System.out.println("Modified from local project.");
     }
 
     public String getMessage() {
